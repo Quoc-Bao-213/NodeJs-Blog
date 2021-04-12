8b87a8104ea95c0c70f6bfa01872e66dbc8a044a
Quoc-Bao-213 <taquocbao.it.99@gmail.com>
2021-04-12 14:31:42 +0700

    Remove lint-staged & husky

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-                const morgan = require('morgan');
-                const handlebars = require('express-handlebars');
-                const path = require('path');
-                const app = express();
-                const port = 3000;
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const path = require('path');
+const app = express();
+const port = 3000;
 
 const route = require('./routes');
 
@@ -14,7 +14,7 @@ app.use(
         extended: true,
     }),
 );
-                app.use(express.json());
+app.use(express.json());
 
 // HTTP logger
 // app.use(morgan('combined'))
@@ -26,12 +26,12 @@ app.engine(
         extname: '.hbs',
     }),
 );
-                app.set('view engine', 'hbs');
-                app.set('views', path.join(__dirname, 'resources', 'views'));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
 // Route init
-       route(app);
+route(app);
 
-                        app.listen(port, () =>
-                            console.log(`Example app listening at http://localhost:${port}`),
-                        );
+app.listen(port, () =>
+    console.log(`Example app listening at http://localhost:${port}`),
+);
