aff5387087674fc206bfd7b99c5107febf501029
Quoc-Bao-213 <taquocbao.it.99@gmail.com>
2021-04-27 15:09:59 +0700

    Add middleware and iconic

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -4,9 +4,29 @@ const { multipleMongooseToObject } = require('../../util/mongoose');
 class UserController {
     // [GET] /user/stored/courses
     storedCourses(req, res, next) {
-        Course.find({})
-            .then((courses) =>
+        let courseQuery = Course.find({});
+
+        if (req.query.hasOwnProperty('_sort')) {
+            courseQuery = courseQuery.sort({
+                [req.query.column]: req.query.type,
+            });
+        }
+
+        Promise.all([courseQuery, Course.countDocumentsDeleted()])
+            .then(([courses, deletedCount]) =>
                 res.render('user/stored-courses', {
+                    deletedCount,
+                    courses: multipleMongooseToObject(courses),
+                }),
+            )
+            .catch(next);
+    }
+
+    // [GET] /user/trash/courses
+    trashCourses(req, res, next) {
+        Course.findDeleted({})
+            .then((courses) =>
+                res.render('user/trash-courses', {
                     courses: multipleMongooseToObject(courses),
                 }),
             )
--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ const morgan = require('morgan');
 const handlebars = require('express-handlebars');
 const path = require('path');
 const methodOverride = require('method-override');
-const app = express();
-const port = 3000;
+
+const sortMiddleware = require('./app/middlewares/SortMiddleware');
 
 const route = require('./routes');
 const db = require('./config/db');
 
 // Connect to DB
 db.connect();
+
+const app = express();
+const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -23,6 +26,9 @@ app.use(express.json());
 
 app.use(methodOverride('_method'));
 
+// Custom middlewares
+app.use(sortMiddleware);
+
 // HTTP logger
 // app.use(morgan('combined'))
 
@@ -33,6 +39,27 @@ app.engine(
         extname: '.hbs',
         helpers: {
             sum: (a, b) => a + b,
+            sortable: (field, sort) => {
+                const sortType = field === sort.column ? sort.type : 'default';
+
+                const icons = {
+                    default: 'oi oi-elevator',
+                    asc: 'oi oi-sort-ascending',
+                    desc: 'oi oi-sort-descending',
+                };
+                const types = {
+                    default: 'desc',
+                    asc: 'desc',
+                    desc: 'asc',
+                };
+
+                const icon = icons[sortType];
+                const type = types[sortType];
+
+                return ` <a href="?_sort&column=${field}&type=${type}">
+                    <span class="${icon}"></span>
+                </a>`;
+            },
         },
     }),
 );
